12e1ed3789d930a3b1512a37dd558c75c0cafef2
ireh1214 <pir@driven.co.kr>
2025-01-02 14:33:46 +0900

    pages 배포

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export", // 정적 파일로 출력
+  assetPrefix: "./", // 상대 경로 설정 (GitHub Pages에서 사용)
+  images: {
+    unoptimized: true // 이미지 최적화 비활성화 (정적 모드에서 필요)
+  },
   webpack(config, { isServer }) {
     config.module.rules.push({
       test: /\.svg$/,
