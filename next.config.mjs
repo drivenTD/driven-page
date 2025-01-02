dfbddfbbded383dc69da79e6cbc503271f2b01df
ireh1214 <pir@driven.co.kr>
2025-01-02 16:12:03 +0900

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // 정적 파일로 출력
-  assetPrefix: "./", // 상대 경로 설정 (GitHub Pages에서 사용)
-  images: {
-    unoptimized: true // 이미지 최적화 비활성화 (정적 모드에서 필요)aa
-  },
   webpack(config, { isServer }) {
+    // SVG 파일을 React 컴포넌트로 변환
     config.module.rules.push({
       test: /\.svg$/,
       use: ["@svgr/webpack"]
     });
 
+    // 개발 환경에서 불필요한 경고를 억제
     if (!isServer) {
       config.infrastructureLogging = {
-        level: "error"
+        level: "error" // 경고를 숨기고 오류만 표시
       };
     }
 
+    // 캐시 전략 수정 (메모리 캐시 사용)
     config.cache = {
-      type: "memory"
+      type: "memory" // 성능을 개선하면서 파일 캐시 대신 메모리 캐시 사용
     };
 
     return config;
